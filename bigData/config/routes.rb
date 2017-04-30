12a55d7489bffd9e22cd160308f261c12625bfaf
santiago <santivaz852@hotmail.com>
2017-04-30 11:10:31 -0500

    root

--- bigData/config/routes.rb
+++ bigData/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :archivos
+    
+    root 'archivos#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
