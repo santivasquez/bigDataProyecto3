36c4f7c594a6d6594c0ba8e976b4465a7c28b1cf
santiago <santivaz852@hotmail.com>
2017-05-03 22:17:18 -0500

    descendente

--- guten/app/controllers/busquedas_controller.rb
+++ guten/app/controllers/busquedas_controller.rb
@@ -2,7 +2,7 @@ class BusquedasController < ApplicationController
   def indice
   	@documentos = []
     if params[:busque].present?
-      @documentos = Gutenberg.where(word: params[:busque]).order_by(numero: :des).limit(20)
+      @documentos = Gutenberg.where(word: params[:busque]).order_by(numero: :desc).limit(20)
     end
   end
 end
